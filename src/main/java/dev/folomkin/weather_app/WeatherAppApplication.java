17f679459f81f2cd6805352d74fa410c353cbb57
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2026-09-13 19:52:12 +0300

    init app

---
+++ src/main/java/dev/folomkin/weather_app/WeatherAppApplication.java
@@ -0,0 +1,13 @@
+package dev.folomkin.weather_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/folomkin/weather_app/WeatherAppApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.folomkin.weather_app;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
